8fec26e519a3d2bd28ab6a474b1309e2a0fcefc4
adellinekania <adellinekaniasetiyawan@gmail.com>
2025-12-01 14:59:35 +0700

    add delay before build info merge

--- src/main/kotlin/org/octopusden/octopus/artifactory/npm/core/service/impl/NpmBuildInfoIntegrationServiceImpl.kt
+++ src/main/kotlin/org/octopusden/octopus/artifactory/npm/core/service/impl/NpmBuildInfoIntegrationServiceImpl.kt
@@ -7,6 +7,7 @@ import org.octopusden.octopus.artifactory.npm.core.service.ArtifactoryBuildInfoS
 import org.octopusden.octopus.artifactory.npm.core.service.JFrogNpmCliService
 import org.octopusden.octopus.artifactory.npm.core.service.NpmBuildInfoIntegrationService
 import org.slf4j.LoggerFactory
+import java.util.concurrent.TimeUnit
 
 class NpmBuildInfoIntegrationServiceImpl(
     private val jfrogNpmCliService: JFrogNpmCliService,
@@ -38,6 +39,12 @@ class NpmBuildInfoIntegrationServiceImpl(
         val npmBuildInfo = buildInfoService.getBuildInfo(buildInfoConfig.npmBuildName, buildInfoConfig.buildNumber)
         val mergedBuildInfo = buildInfoService.mergeBuildInfo(mavenBuildInfo, npmBuildInfo)
         buildInfoService.uploadBuildInfo(mergedBuildInfo)
+
+        // TODO: Implement proper check for Xray scan status/availability before uploading merged build info to avoid race conditions
+        logger.warn(
+            "Waiting for 1 minute before uploading merged build info to prevent Xray indexing race condition"
+        )
+        Thread.sleep(TimeUnit.MINUTES.toMillis(1))
 
         if (buildInfoConfig.cleanupNpmBuildInfo) {
             buildInfoService.deleteBuildInfo(buildInfoConfig.npmBuildName, listOf(buildInfoConfig.buildNumber))
