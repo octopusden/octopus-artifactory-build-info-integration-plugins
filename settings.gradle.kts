8ea9d88913608485ac3427df3c291ed2fc24e3ed
adellinekania <adellinekaniasetiyawan@gmail.com>
2026-02-04 09:26:08 +0700

    use providers.gradleProperty for plugin version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,15 @@
 pluginManagement {
+    val pluginVersion = object {
+        val kotlin = providers.gradleProperty("kotlin.version")
+        val nexusPublish = providers.gradleProperty("nexus-publish.version")
+        val jfrogArtifactory = providers.gradleProperty("jfrog-artifactory.version")
+        val ocTemplate = providers.gradleProperty("octopus-oc-template.version")
+    }
     plugins {
-        kotlin("jvm") version extra["kotlin.version"] as String
-        id("io.github.gradle-nexus.publish-plugin") version extra["nexus-publish.version"] as String
-        id("com.jfrog.artifactory") version extra["jfrog-artifactory.version"] as String
-        id("org.octopusden.octopus.oc-template") version extra["octopus-oc-template.version"] as String
+        kotlin("jvm") version pluginVersion.kotlin.get()
+        id("io.github.gradle-nexus.publish-plugin") version pluginVersion.nexusPublish.get()
+        id("com.jfrog.artifactory") version pluginVersion.jfrogArtifactory.get()
+        id("org.octopusden.octopus.oc-template") version pluginVersion.ocTemplate.get()
     }
 }
 
